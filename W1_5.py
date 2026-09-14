920972b953c92ebc41d9a2cc1da45d319c828f83
Phat Nguyen <tanphat.ng1107@gmail.com>
2026-09-14 10:22:18 +0300

    hei

---
+++ W1_5.py
@@ -0,0 +1,15 @@
+print("Calculate the area of a wall.")
+Feed=input("Enter the width in meters:")
+Width = int(Feed)
+Feed=input("Enter the length in meters:")
+Height = int(Feed)
+print(f"Width is {Width} m and height is {Height} m.")
+Area=Width*Height
+print(f"The wall will be {Area} square meters.")
+#-----------------------------------------------------------
+Width = int(input("Enter the width in meters: "))
+Height = int(input("Enter the length in meters: "))
+
+print(f"Width is {Width} m and height is {Height} m.")
+Area = Width * Height
+print(f"The wall will be {Area} square meters.")
